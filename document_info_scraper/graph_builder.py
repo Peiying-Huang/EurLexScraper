4a0f2633e7d66c256ef717b1bb0a0c38845031f0
Peiying_Huang <hpyclara@gmail.com>
2026-04-29 15:02:39 +0200

    Add document info scraper

--- document_info_scraper/graph_builder.py
+++ document_info_scraper/graph_builder.py
@@ -1,8 +1,6 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from document_info_scaper import DocumentInfoScraper
-import spacy
-import re
+
 
 class GraphBuilder:
     def __init__(self, url):
